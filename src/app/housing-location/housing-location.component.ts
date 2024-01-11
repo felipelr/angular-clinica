ae3767b82bc5b37295ad19639931923dc5a9841f
Felipe Windows 11 <felipe.lima.flr@gmail.com>
2024-01-11 17:56:03 -0300

    feat: first components

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../types/housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
